2aef9fb609b31131aaba4f97045827daac96ba46
Shafayathub <shafayat.ms@gmail.com>
2025-02-19 15:29:13 +0600

    path printing example

---
+++ path_printing.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[1005];
+bool vis[1005];
+int level[1005];
+int par[1005];
+
+void bfs(int src){
+    queue<int> q;
+    q.push(src);
+    vis[src]= true;
+    level[src] = 0;
+    while(!q.empty()){
+        int parent = q.front();
+        q.pop();
+        for(int child : adj_list[parent]){
+            if(!vis[child]){
+                q.push(child);
+                vis[child]= true;
+                level[child] = level[parent] + 1;
+                par[child] = parent;
+            }
+        }
+    }
+}
+
+int main() {
+
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+    memset(par, -1, sizeof(par));
+    int src, dst;
+    cin >> src >> dst;
+    bfs(src);
+
+    int node = dst;
+    while(node != -1){
+        cout << node << " ";
+        node = par[node];
+    }
+
+    
+    return 0;
+}
